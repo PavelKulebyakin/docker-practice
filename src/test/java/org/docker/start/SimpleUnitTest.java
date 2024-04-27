e35ed38c8aa23087070713c97b926311f74000d9
Pavel Kulebyakin <pavel.kulebyakin.study@gmail.com>
2024-04-27 23:09:17 +0300

    Add simple calculator class with test

---
+++ src/test/java/org/docker/start/SimpleUnitTest.java
@@ -0,0 +1,20 @@
+package org.docker.start;
+
+import org.docker.start.service.CalculatorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class SimpleUnitTest {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @Test
+    public void testSum() {
+        assertThat(calculatorService.sum(3, 4)).isEqualTo(7);
+    }
+}
